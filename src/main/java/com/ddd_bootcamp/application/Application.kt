c310b3d957b9d92e854469b4c6ce73a3be4f712a
Pedro Ruiz <pedroantonio.ruiz@adevinta.com>
2023-01-17 14:27:18 +0100

    printCart function

--- src/main/java/com/ddd_bootcamp/application/Application.kt
+++ src/main/java/com/ddd_bootcamp/application/Application.kt
@@ -20,16 +20,25 @@ object Application {
         )
         cart1.add(headphone)
         cart2.add(headphone)
-        cart1.remove(headphone.product.name)
-        println("Cart1 = $cart1")
-        println("Cart2 = $cart2")
-        val cartItems = cart1.items.joinToString("\n\t") { it.toString() }
-        val removedCartItems = cart1.removedItems.joinToString("\n\t") { it.toString() }
-        println("----------------------------------------")
-        println("Cart Items = \n$cartItems")
-        println("----------------------------------------")
-        println("Removed Cart Items = \n$removedCartItems")
-        println("----------------------------------------")
+        printCart("Cart1", cart1)
+        printCart("Cart2", cart2)
         println("Are cart1 & cart2 the same? ${cart1 == cart2}")
     }
+}
+
+private fun printCart(cartName: String, cart: Cart) {
+    println("----------------------------------------")
+    println("----------------- $cartName ----------------")
+    println("----------------------------------------")
+    println("Cart = $cart")
+    val cartItems = cart.items.joinToString("\n\t") { it.toString() }
+    val removedCartItems = cart.removedItems.joinToString("\n\t") { it.toString() }
+    println("----------------------------------------")
+    println("Cart Items = \n$cartItems")
+    println("----------------------------------------")
+    println("Removed Cart Items = \n$removedCartItems")
+    println("----------------------------------------")
+    println("----------------------------------------")
+    println("----------------- $cartName End ------------")
+    println("----------------------------------------")
 }
